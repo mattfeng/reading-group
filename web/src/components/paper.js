55bc370f0d06004c0ebd723336398fc829fd9596
Matthew Feng <mattfeng@mit.edu>
2021-12-30 16:17:37 -0500

    better paper info

--- web/src/components/paper.js
+++ web/src/components/paper.js
@@ -2,23 +2,33 @@ import React from "react"
 
 import * as styles from "../styles/paper.module.scss"
 
-const Tag = ({ tag }) => {
-  return <span className={styles.tag}>{tag}</span>
+const Tags = ({ tags }) => {
+  return <div className={styles.tags}>tags: {tags.join(", ")}</div>
 }
 
 const Paper = ({ paper }) => {
   const { title, url, anno, tags, year, journal } = paper
 
+  let display
+
+  if (journal && year) {
+    display = `${title} (${journal}, ${year})`
+  } else if (year) {
+    display = `${title} (${year})`
+  } else {
+    display = `${title}`
+  }
+
   return (
     <div className={styles.paperContainer}>
       <p>
         <a target="_blank" rel="noreferrer" href={url}>
-          {title} {year ? `(${year})` : null}
+          {display}
         </a>
       </p>
       {anno ? <p className={styles.annotation}>{anno}</p> : null}
 
-      {tags ? tags.map(tag => <Tag tag={tag} />) : null}
+      {tags ? <Tags tags={tags} /> : null}
     </div>
   )
 }
